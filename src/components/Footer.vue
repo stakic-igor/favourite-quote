97d3c2914a6627f1b37c1bd8298eb00fde0558ff
Igor Stakic <stakic_igor@yahoo.com>
2025-10-01 22:14:51 +0200

    Add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+    <footer class="w-full p-4 bg-gray-800 text-white text-center">
+        <p>Inspirational NINJA quotes provided by <a href="https://api-ninjas.com/" target="_blank">API Ninja</a></p>
+    </footer>
+</template>
