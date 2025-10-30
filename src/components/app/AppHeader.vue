2d9ec4332a505be398a92c67177dab5c37edbaef
Igor Stakic <stakic_igor@yahoo.com>
2025-10-30 22:56:38 +0100

    reconstruct app header

---
+++ src/components/app/AppHeader.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <header class="flex flex-col w-full p-4">
+    <img 
+        src="../../assets/ninja.png" 
+        alt="logo" 
+        class="h-30 self-center" 
+    />
+    <h1 class="text-2xl font-bold text-center">Ninja Quotes</h1>
+  </header>
+</template>
