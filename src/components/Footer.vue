73237268ab58a927880a727c18632d07a56f28b4
Igor Stakic <stakic_igor@yahoo.com>
2025-10-30 22:22:45 +0100

    feat(): restructure footer

--- src/components/Footer.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <footer class="w-full p-4 bg-gray-800 text-white text-center">
-        <p>Inspirational NINJA quotes provided by <a href="https://api-ninjas.com/" target="_blank">API Ninja</a></p>
-    </footer>
-</template>
---
+++ src/components/app/AppFooter.vue
@@ -0,0 +1,6 @@
+<template>
+    <footer class="w-full p-4 border-t border-gray-400 text-black text-center flex flex-row items-center">
+        <img src="../../assets/ninja.png" alt="logo" class="h-10" />
+        <p>quotes provided by <a href="https://api-ninjas.com/" target="_blank">API Ninja</a></p>
+    </footer>
+</template>
